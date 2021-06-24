80d876ce162b229c2933c60750fbb191560375c2
zhongshun <zszhang@outlook.com>
2021-06-24 15:36:13 -0400

    parameter issue

--- set_params_quantitative.m
+++ set_params_quantitative.m
@@ -15,7 +15,7 @@ Lookahead = 5;          % planning horizon
 T_execution = 9;       % episode duration
 Discount_factor = 0.95;
 
-USE_HEURISTIC = true;         
+USE_HEURISTIC = flase;         
 heur_penalty_std = 1;        % std of penalty to use for the heuristic
 heur_agent_asset_weight = 1;            % how much weight to give the asset penalty in the heuristic
 heur_agent_detection_weight = 1;     % how much weight to give the opponent detecting the agent penalty in the heuristic
@@ -23,7 +23,7 @@ heur_agent_detection_weight = 1;     % how much weight to give the opponent dete
 Number_of_Assets = 2;    
 Asset_set = [2 2;2 10;10 10; 10 2; 3 4; 3 8;9 4;9 8;];
 
-environment = read_vertices_from_file('./Environments/Env_C.environment');  %The environment and its boundary
+environment = read_vertices_from_file('./Environments/Env_B.environment');  %The environment and its boundary
 environment_min_x = min(environment{1}(:,1));
 environment_max_x = max(environment{1}(:,1));
 environment_min_y = min(environment{1}(:,2));
